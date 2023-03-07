7fd489321f3b02b116a944d6b4a3d4c7d9e8397f
Emmanuel Karingi <emmanuelkaringi@gmail.com>
2023-03-07 09:39:52 +0000

    fix app send data issue

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -20,7 +20,6 @@ from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 # HoneyComb
 from opentelemetry import trace
@@ -38,6 +37,9 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
+
+# X-Ray
+XRayMiddleware(app, xray_recorder)
 
 # Initialize automatic instrumentation of HoneyComb with Flask
 FlaskInstrumentor().instrument_app(app)
